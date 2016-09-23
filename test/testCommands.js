b273ac6a7f34f09393ba9ec79369a0e28212289c
allavoie <alain.lavoie@dexto.ca>
2016-09-22 20:47:09 -0400

    Ok

--- test/testCommands.js
+++ test/testCommands.js
@@ -7,20 +7,28 @@ const sinon = require("sinon");
 
 describe("WOSH - Wo shell commander", () => {
     describe("Support multiple shell context and syntax", ()=> {
-        it("should support echo command", (done) => {
+
+        it("should support a script file command", (done) => {
             const spawn = require('child_process').spawn;
             let runSh = spawn(__dirname + '/echo-simple.sh');
-            runSh.stdout.on('data', (data)=>{
-                console.log(`d=${data}`);
-            });
-            runSh.stderr.on('data', (data)=>{
-                console.log(`e=${data}`);
-            });
-            runSh.on('close', (code)=>{
-                console.log(`c=${code}`);
+            runSh.stdout.on('data', (data)=>{ assert.equal("hello\n", `${data}`, "echo string found"); });
+            runSh.stderr.on('data', (data)=>{ console.log(`e=${data}`); });
+            runSh.on('close', (code)=>{ assert.equal(code, 0, "No error code (0)");
                 done();
             });
         });
+
+        it("should support a simple echo command (with single quote)", (done) => {
+            const exec = require("child_process").exec;
+            const SINGLE_QUOTE = "'";
+            const str = "hello world";
+            const helloWorld=SINGLE_QUOTE + "hello world" + SINGLE_QUOTE;
+            exec("echo " + helloWorld, (error, stdout, stderr)=>{
+                assert.equal(str + "\n", stdout, "");
+                done();
+            });
+        });
+
         it("should support awk command", (done) => {
             done();
         });
