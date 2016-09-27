d08af674a4144f459389da991a8320669f01110c
allavoie <alain.lavoie@dexto.ca>
2016-09-26 20:17:40 -0400

    Ok

--- test/testCommands.js
+++ test/testCommands.js
@@ -6,6 +6,66 @@ const _ = require('lodash');
 const sinon = require("sinon");
 
 describe("WOSH - Wo shell commander", () => {
+
+
+    describe("Accept input from pipe", ()=> {
+
+        /*
+        * @api
+        * @parameters
+        * @url http://www.computerhope.com/unix/usort.htm  -- To explain the type of input (ending with null separator)
+        * @url http://stackoverflow.com/questions/35004492/node-child-process-spawn-multiple-commands
+        * @intention: A component shall suuport different source of input (stdin, pipe, file, wostore)
+        *
+        */
+
+        it("should deal with command [>cat ./simple-file.json | ../bin/wosh sort]", (done) => {
+            const spawn = require('child_process').spawn;
+            let aCmd = "echo";
+            let args = ["Aie, hello\nyou"];
+            let runSh = spawn(aCmd, args);
+            runSh.stdout.on('data', (data)=>{
+                console.log("Echo output:", `${data}`);
+            });
+            runSh.stderr.on('data', (data)=>{ console.log(`e=${data}`); });
+            runSh.on('close', (code)=>{ assert.equal(code, 0, "No error code (0)");
+                done();
+            });
+        });
+
+        it("should support a simple [>echo 'hello\nyou'] - command with special characters", (done) => {
+            const spawn = require('child_process').spawn;
+            let aCmd = "echo";
+            let args = ["Aie, hello\nyou"];
+            let runSh = spawn(aCmd, args);
+            runSh.stdout.on('data', (data)=>{
+                console.log("Echo output:", `${data}`);
+            });
+            runSh.stderr.on('data', (data)=>{ console.log(`e=${data}`); });
+            runSh.on('close', (code)=>{ assert.equal(code, 0, "No error code (0)");
+                done();
+            });
+        });
+
+        it("should support a simple [echo 'hello world']", (done) => {
+            const exec = require("child_process").exec;
+            const SINGLE_QUOTE = "'";
+            const str = "hello world";
+            const helloWorld=SINGLE_QUOTE + "hello world" + SINGLE_QUOTE;
+            exec("echo " + helloWorld, (error, stdout, stderr)=>{
+                assert.equal(str + "\n", stdout, "");
+                done();
+            });
+        });
+
+        it("should support awk command", (done) => {
+            done();
+        });
+
+    });
+
+
+
     describe("Support multiple shell context and syntax", ()=> {
 
         it("should support a script file command [echo-simple.sh]", (done) => {
